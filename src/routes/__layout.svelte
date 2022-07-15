746ebddaa77dec23f855659a2989282b33852d46
mattcroat <mattcroat.dev@gmail.com>
2022-07-15 17:55:12 +0200

    style: Global styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,40 @@
 <script lang="ts">
+	import 'open-props/style'
 	import '../app.css'
 </script>
 
-<a href="/">Home</a>
-<a href="/shows">Shows</a>
+<header>
+	<h1>Track Shows</h1>
+	<nav>
+		<a href="/">Home</a>
+		<a href="/shows">Progress</a>
+	</nav>
+</header>
 
-<slot />
+<main>
+	<slot />
+</main>
+
+<style>
+	header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	h1 {
+		font-size: var(--font-size-3);
+	}
+
+	nav {
+		display: flex;
+		justify-content: end;
+		gap: var(--size-3);
+	}
+
+	main {
+		display: grid;
+		justify-content: center;
+		justify-items: center;
+	}
+</style>
